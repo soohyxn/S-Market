7797a63ddad36126305eba81e10027f35d94ef64
soohyun <soohyun6879@naver.com>
2021-11-12 20:51:21 +0900

    제조사, 카테고리 모델 생성 및 음식 모델과 연결하기

--- foods/admin.py
+++ foods/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from foods.models import Food
+from foods.models import Food, Manufacturer, Category
 
 admin.site.register(Food, MarkdownxModelAdmin)
+admin.site.register(Manufacturer)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
--- foods/models.py
+++ foods/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdown
 
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=50)
+    contact = models.CharField(max_length=15)
+    domain = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name}'
+
 class Food(models.Model):
     name = models.CharField(max_length=30)
     content = MarkdownxField()
@@ -11,6 +30,9 @@ class Food(models.Model):
     price = models.IntegerField()
     calorie = models.IntegerField()
     sales = models.IntegerField(default=0)
+
+    manufacturer = models.ForeignKey(Manufacturer, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}] {self.name}'
