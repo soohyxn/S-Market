0c4a14508acb6c912a11cfeb3ab2b163e20687f1
soohyun <soohyun6879@naver.com>
2021-11-14 16:07:42 +0900

    카테고리 페이지 만들기

--- foods/urls.py
+++ foods/urls.py
@@ -4,4 +4,5 @@ from foods import views
 urlpatterns = [
     path('', views.FoodList.as_view()),
     path('<int:pk>/', views.FoodDetail.as_view()),
+    path('category/<str:slug>/', views.category_page),
 ]
--- foods/views.py
+++ foods/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from foods.models import Food
+from foods.models import Food, Category
 
 
 class FoodList(ListView):
     model = Food
-    ordering = '-sales'
+    ordering = 'pk'
 
 class FoodDetail(DetailView):
     model = Food
+
+def category_page(request, slug):
+    category = Category.objects.get(slug=slug)
+    food_list = Food.objects.filter(category=category)
+
+    return render(request, 'foods/food_list.html', {'food_list': food_list, 'category': category})
