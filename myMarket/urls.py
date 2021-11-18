d5e016a85353af65a3bbe6d5bd726df99a9471f3
soohyun <soohyun6879@naver.com>
2021-11-18 20:50:22 +0900

    로그인 기능 추가

--- myMarket/urls.py
+++ myMarket/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('foods.urls')),
+    path('accounts/', include('allauth.urls')),
     path('markdownx/', include('markdownx.urls')),
 ]
 
