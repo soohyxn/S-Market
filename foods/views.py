15b611f5dba319093b549c8d5ade14722b273ecc
soohyun <soohyun6879@naver.com>
2021-11-22 12:11:10 +0900

    상품 목록 페이지에 pagination 적용하기

--- foods/views.py
+++ foods/views.py
@@ -54,6 +54,7 @@ class FoodUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class FoodList(ListView):
     model = Food
     ordering = 'pk'
+    paginate_by = 6
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(FoodList, self).get_context_data()
